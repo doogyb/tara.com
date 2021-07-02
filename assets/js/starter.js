6ab5f8de286ce7aaae4f2b70a43303139d1fde46
doogyb <samueldoogan@gmail.com>
2021-07-02 12:07:29 +0100

    OK so now we remove the packages (as they don't get built using this starter repo) and put them in the assets folder

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -17,5 +17,5 @@
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
 import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
 import "../../node_modules/bootstrap/js/dist/collapse.js";
